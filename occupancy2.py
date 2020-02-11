2daf928fdb8b5ac2e8b8e432815115bcd3aa64a9
Wang Huachen <huachenw24@gmail.com>
2020-02-11 17:49:07 +0800

    updated occupancy2.py for homework testbench.py for electrical homework 11 feb

--- occupancy2.py
+++ occupancy2.py
@@ -27,7 +27,6 @@ def callback(msg, tfBuffer):
     # log the info
     rospy.loginfo('Width: %i Height: %i',msg.info.width,msg.info.height)
     rospy.loginfo('Unmapped: %i Unoccupied: %i Occupied: %i Total: %i', occ_counts[0][0], occ_counts[0][1], occ_counts[0][2], total_bins)
-
     # find transform to convert map coordinates to base_link coordinates
     # lookup_transform(target_frame, source_frame, time)
     trans = tfBuffer.lookup_transform('base_link', 'map', rospy.Time(0))
@@ -37,19 +36,26 @@ def callback(msg, tfBuffer):
     orientation_list = [trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z, trans.transform.rotation.w]
     (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
     rospy.loginfo(['Yaw: R: ' + str(yaw) + ' D: ' + str(np.degrees(yaw))])
-
+    trans2 = tfBuffer.lookup_transform('map', 'base_link', rospy.Time(0))
+    cur_pos = trans2.transform.translation
+    map_res = msg.info.resolution
+    map_origin = msg.info.origin.position
+    grid_x = (cur_pos.x - map_origin.x) / map_res
+    grid_y = (cur_pos.y - map_origin.y) / map_res
     # make occdata go from 0 instead of -1, reshape into 2D
     oc2 = occdata + 1
     # set all values above 1 (i.e. above 0 in the original map data, representing occupied locations)
     oc3 = (oc2>1).choose(oc2,2)
     # reshape to 2D array using column order
     odata = np.uint8(oc3.reshape(msg.info.width,msg.info.height,order='F'))
+    odata[grid_x][grid_y] = 0
     # create image from 2D array using PIL
     img = Image.fromarray(odata)
     # rotate by 180 degrees to invert map so that the forward direction is at the top of the image
     rotated = img.rotate(np.degrees(yaw)+180)
+    translated = rotated.transform(rotated.size, Image.AFFINE, (1, 0, 0, 0, 1, trans.transform.translation.x * 100))
     # show image using grayscale map
-    plt.imshow(rotated,cmap='gray')
+    plt.imshow(translated,cmap='gray')
     plt.draw_all()
     # pause to make sure the plot gets created
     plt.pause(0.00000000001)
---
+++ testbench.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  5 20:54:17 2020
+
+@author: huachen
+"""
+
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+import numpy as np
+import time
+import RPi.GPIO as GPIO
+
+laser_range = np.array([])
+occdata = []
+yaw = 0.0
+rotate_speed = 0.3
+linear_speed = 0.03
+stop_distance = 1
+occ_bins = [-1, 0, 100, 101]
+front_angle = 5
+front_angles = range(-front_angle,front_angle+1,1)
+
+def get_laserscan(msg):
+    global laser_range
+
+    # create numpy array
+    laser_range = np.array([msg.ranges])
+
+def pick_direction():
+    global laser_range
+
+    rospy.loginfo(['Moving forward'])
+    # publish to cmd_vel to move TurtleBot
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+    # stop moving
+    twist = Twist()
+    twist.linear.x = 0.0
+    twist.angular.z = 0.0
+    time.sleep(1)
+    pub.publish(twist)
+
+    # start moving
+    rospy.loginfo(['Start moving'])
+    twist.linear.x = linear_speed
+    twist.angular.z = 0.0
+    # not sure if this is really necessary, but things seem to work more
+    # reliably with this
+    time.sleep(1)
+    pub.publish(twist)
+
+
+def mover():
+    global laser_range
+
+    rospy.init_node('mover', anonymous=True)
+
+    # subscribe to LaserScan data
+    rospy.Subscriber('scan', LaserScan, get_laserscan)
+
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+    rate = rospy.Rate(5) # 5 Hz
+
+    # find direction with the largest distance from the Lidar
+    # rotate to that direction
+    # start moving
+    pick_direction()
+
+    while not rospy.is_shutdown():
+        # check distances in front of TurtleBot
+        lr2 = laser_range[0,front_angles]
+        # distances beyond the resolution of the Lidar are returned
+        # as zero, so we need to exclude those values
+        lr20 = (lr2!=0).nonzero()
+        # find values less than stop_distance
+        lr2i = (lr2[lr20]<float(stop_distance)).nonzero()
+
+        # if the list is not empty
+        if(len(lr2i[0])>0):
+            rospy.loginfo(['Stop!'])
+            # stop moving
+            twist = Twist()
+            twist.linear.x = 0.0
+            twist.angular.z = 0.0
+            time.sleep(1)
+            pub.publish(twist)
+            # fire servo
+            GPIO.setmode(GPIO.BOARD)
+            servo_pin = 40
+            GPIO.setup(servo_pin, GPIO.OUT)
+            p = GPIO.PWM(servo_pin, 50)
+            p.start(7.5)
+            time.sleep(1)
+            p.ChangeDutyCycle(5)
+            time.sleep(1)
+            p.stop()
+            GPIO.cleanup()
+            # fire solenoid
+            GPIO.setmode(GPIO.BOARD)
+            solenoid_pin = 38
+            GPIO.setup(solenoid_pin, GPIO.OUT)
+            GPIO.output(solenoid_pin, GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(solenoid_pin, GPIO.LOW)
+            time.sleep(1)
+            GPIO.cleanup()
+            rate.sleep()
+
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        mover()
+    except rospy.ROSInterruptException:
+        pass
