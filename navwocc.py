2c604d893a0f6419b2ec50d4492b11e2d87d30e7
Wang Huachen <huachenw24@gmail.com>
2020-04-10 17:33:05 +0800

    Almost complete code Todo: check perpendicular travel check end

--- navwocc.py
+++ navwocc.py
@@ -23,9 +23,9 @@ occdata = np.array([])
 yaw = 0.0
 rotate_speed = 0.5
 linear_speed = 0.1
-stop_distance = 0.25
+stop_distance = 0.5
 occ_bins = [-1, 0, 100, 101]
-front_angle = 30
+front_angle = 25
 front_angles = range(-front_angle,front_angle+1,1)
 
 
@@ -54,10 +54,7 @@ class Point(object):
         return int((math.sqrt(self.x**2 + self.y**2)))
 
     def iswithin(self, other):
-        return (other.x-10 <= self.x <= other.x+10) and (other.y-10 <= self.y <= other.y+10)
-
-
-dir4 = [Point((0, -1)), Point((0, 1)), Point((1, 0)), Point((-1, 0))]
+        return (other.x-3 <= self.x <= other.x+3) and (other.y-3 <= self.y <= other.y+3)
 
 
 def get_odom_dir(msg):
@@ -114,10 +111,12 @@ def findpoint():
     
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
     _, bina1 = cv2.threshold(img, 125, 255, cv2.THRESH_BINARY)
     _, contours, _ = cv2.findContours(bina1, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     _, nowhite = cv2.threshold(img, 255, 0, cv2.THRESH_BINARY)
     rgb1 = cv2.cvtColor(nowhite, cv2.COLOR_GRAY2BGR)
+
     for contour in contours:
         cv2.drawContours(rgb1, contour, -1, (255,255,255),1)
 
@@ -134,52 +133,114 @@ def findpoint():
     ero = cv2.erode(diff,kernel,iterations=3)
     img = cv2.cvtColor(ero, cv2.COLOR_BGR2GRAY)
     _, img = cv2.threshold(img, 148, 255, cv2.THRESH_BINARY)
-    twoff = np.where(img == 255)
-    tup = list(zip(twoff[0], twoff[1]))
-    ptmin = Point(tup[0])
-    ptmax = Point(tup[-1])
-    ptdif = ptmax - ptmin
-    ptmid = ptmax.midpoint(ptmin)
-    perp = Point((-ptdif.y, ptdif.x))
-    ptperp = ptmid - perp*(8.0/perp.length())
-    ptperp2 = ptmid + perp*(8.0/perp.length())
 
-    if bina1[ptperp.x][ptperp.y] == 255:
-        q.append(ptperp)
-        q.append(ptperp2)
-        img[ptperp.x][ptperp.y] = 255
-    else: 
-        q.append(ptperp2)
-        q.append(ptperp)
-        img[ptperp2.x][ptperp2.y] = 255
 
-    for i in q:
-        print(i.x, i.y)
+    kern1 = np.array(([1, -1, -1], [-1, 1, -1], [-1,-1,-1]), dtype='int')
+    kern2 = np.array(([-1, 1, -1], [-1, 1, -1], [-1,-1,-1]), dtype='int')
+    kern3 = np.array(([-1, -1, 1], [-1, 1, -1], [-1,-1,-1]), dtype='int')
+    kern4 = np.array(([-1, -1, -1], [1, 1, -1], [-1,-1,-1]), dtype='int')
+    kern6 = np.array(([-1, -1, -1], [-1, 1, 1], [-1,-1,-1]), dtype='int')
+    kern7 = np.array(([-1, -1, -1], [-1, 1, -1], [1,-1,-1]), dtype='int')
+    kern8 = np.array(([-1, -1, -1], [-1, 1, -1], [-1,1,-1]), dtype='int')
+    kern9 = np.array(([-1, -1, -1], [-1, 1, -1], [-1,-1,1]), dtype='int')
+
+    out1 = cv2.morphologyEx(img, cv2.MORPH_HITMISS, kern1)
+    out2 = cv2.morphologyEx(img, cv2.MORPH_HITMISS, kern2)
+    out3 = cv2.morphologyEx(img, cv2.MORPH_HITMISS, kern3)
+    out4 = cv2.morphologyEx(img, cv2.MORPH_HITMISS, kern4)
+    out6 = cv2.morphologyEx(img, cv2.MORPH_HITMISS, kern6)
+    out7 = cv2.morphologyEx(img, cv2.MORPH_HITMISS, kern7)
+    out8 = cv2.morphologyEx(img, cv2.MORPH_HITMISS, kern8)
+    out9 = cv2.morphologyEx(img, cv2.MORPH_HITMISS, kern9)
+
+    out = cv2.bitwise_or(out1, out2)
+    out = cv2.bitwise_or(out, out3)
+    out = cv2.bitwise_or(out, out4)
+    out = cv2.bitwise_or(out, out6)
+    out = cv2.bitwise_or(out, out7)
+    out = cv2.bitwise_or(out, out8)
+    out = cv2.bitwise_or(out, out9)
+
+    endpts = np.where(out == 255)
+
+    try:
+        ptmin = Point((endpts[0][0], endpts[1][0]))
+    except IndexError as e:
+        cv2.imwrite('mazemap.png',odata)
+        rospy.signal_shutdown('Mapping complete')
+
+    try:
+        ptmax = Point((endpts[0][1], endpts[1][1]))
+    except IndexError as e:
+        rospy.logwarn('Only 1 endpoint found')
+        dist = ptmin - loc
+        norm = Point((-dist.y, dist.x))
+        first = loc - norm*(10.0/norm.length())
+        first2 = loc + norm*(10.0/norm.length())
+
+        if bina1[first.x][first.y] == 255:
+            q.append(first)
+            q.append(ptmin)
+        else:
+            q.append(first2)
+            q.append(ptmin)
+    else:
+        ptdif = ptmax - ptmin
+        ptmid = ptmax.midpoint(ptmin)
+        perp = Point((-ptdif.y, ptdif.x))
+        ptperp = ptmid - perp*(10.0/perp.length())
+        ptperp2 = ptmid + perp*(10.0/perp.length())
+        dist = ptperp - loc
+        norm = Point((-dist.y, dist.x))
+        first = loc + norm*(30.0/norm.length())
+        if bina1[first.x][first.y] != 255:
+            first = loc - norm*(10.0/norm.length())
+        dist2 = ptperp2 - loc
+        norm2 = Point((-dist2.y, dist2.x))
+        first2 = loc + norm2*(30.0/norm2.length())
+        if bina1[first2.x][first2.y] != 255:
+            first2 = loc - norm2*(10.0/norm2.length())
+
+        if bina1[ptperp.x][ptperp.y] == 255:
+            q.append(first)
+            q.append(ptperp)
+            q.append(ptperp2)
+            #img[ptperp.x][ptperp.y] = 255
+        else: 
+            q.append(first2)
+            q.append(ptperp2)
+            q.append(ptperp)
+            #img[ptperp2.x][ptperp2.y] = 255
 
     img[loc.x][loc.y] = 255
-    cv2.imshow("final", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    rospy.loginfo('Current Position: (' + str(loc.x) + ', ' + str(loc.y) + ')')
+    rospy.loginfo('Target Position: (' + str(q[0].x) + ', ' + str(q[0].y) + ')')
+    #cv2.imshow("final", img)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
 
 def movebot(goal):
-    global loc
+    global loc, laser_range
 
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     rate = rospy.Rate(10)
     twist = Twist()
-    dist = goal - loc
-    s = dist.length()
     twist.linear.x = 0.15
     twist.angular.z = 0.0
     pub.publish(twist)
-    time.sleep(s/10)
+    lri = (laser_range[front_angles]<float(stop_distance)).nonzero()
+
+    while len(lri[0])==0:
+        if loc.iswithin(goal):
+            break
+        lri = (laser_range[front_angles]<float(stop_distance)).nonzero()
+        rate.sleep()
     
     # stop moving
     twist = Twist()
     twist.linear.x = 0.0
-    twist.angular.z = 0.0
-    time.sleep(1)
+    #twist.angular.z = 0.0
     pub.publish(twist)
 
 
@@ -198,7 +259,7 @@ def rotatebot(rot_angle):
     # get current yaw angle
     current_yaw = np.copy(yaw)
     # log the info
-    rospy.loginfo(['Current: ' + str(math.degrees(current_yaw))])
+    #rospy.loginfo('Current Yaw: ' + str(math.degrees(current_yaw)))
     # we are going to use complex numbers to avoid problems when the angles go from
     # 360 to 0, or from -180 to 180
     c_yaw = complex(math.cos(current_yaw),math.sin(current_yaw))
@@ -206,7 +267,7 @@ def rotatebot(rot_angle):
     target_yaw = rot_angle
     # convert to complex notation
     c_target_yaw = complex(math.cos(target_yaw),math.sin(target_yaw))
-    rospy.loginfo(['Desired: ' + str(math.degrees(cmath.phase(c_target_yaw)))])
+    rospy.loginfo('Desired Yaw: ' + str(math.degrees(cmath.phase(c_target_yaw))))
     # divide the two complex numbers to get the change in direction
     c_change = c_target_yaw / c_yaw
     # get the sign of the imaginary component to figure out which way we have to turn
@@ -228,7 +289,7 @@ def rotatebot(rot_angle):
         current_yaw = np.copy(yaw)
         # get the current yaw in complex form
         c_yaw = complex(math.cos(current_yaw),math.sin(current_yaw))
-        rospy.loginfo('While Yaw: %f Target Yaw: %f', math.degrees(current_yaw), math.degrees(target_yaw))
+        #rospy.loginfo('While Yaw: %f Target Yaw: %f', math.degrees(current_yaw), math.degrees(target_yaw))
         # get difference in angle between current and target
         c_change = c_target_yaw / c_yaw
         # get the sign to see if we can stop
@@ -236,7 +297,7 @@ def rotatebot(rot_angle):
         # rospy.loginfo(['c_change_dir: ' + str(c_change_dir) + ' c_dir_diff: ' + str(c_dir_diff)])
         rate.sleep()
 
-    rospy.loginfo(['End Yaw: ' + str(math.degrees(current_yaw))])
+    rospy.loginfo('End Yaw: ' + str(math.degrees(current_yaw)))
     # set the rotation speed to 0
     twist.angular.z = 0.0
     # stop the rotation
@@ -263,23 +324,19 @@ def pick_direction():
 
     while q != []:
         s = q.pop()
-        
-        while not loc.iswithin(s):
-            dirr = s - loc
-            angle2s = math.atan2(dirr.y, dirr.x)
-            print("angle: ", angle2s)
-            print("current: ", yaw)
-            rotatebot(angle2s)
-            print("DONE ROTATING")
-            movebot(s)
-            print("DONE MOVING")
+        dirr = s - loc
+        angle2s = math.atan2(dirr.y, dirr.x)
+        rotatebot(angle2s)
+        rospy.loginfo('Rotation Complete')
+        movebot(s)
+        rospy.loginfo('Move Complete')
 
         findpoint()
 
     botspeed.linear.x = 0
     botspeed.angular.z = 0
     pub.publish(botspeed)
-    rospy.logwarn("Reached end")
+    rospy.logwarn("Reached End")
 
     # start moving
     #rospy.loginfo(['Start moving'])
@@ -303,39 +360,17 @@ def stopbot():
 
 
 def closure(mapdata):
-    # This function checks if mapdata contains a closed contour. The function
-    # assumes that the raw map data from SLAM has been modified so that
-    # -1 (unmapped) is now 0, and 0 (unoccupied) is now 1, and the occupied
-    # values go from 1 to 101.
-
-    # According to: https://stackoverflow.com/questions/17479606/detect-closed-contours?rq=1
-    # closed contours have larger areas than arc length, while open contours have larger
-    # arc length than area. But in my experience, open contours can have areas larger than
-    # the arc length, but closed contours tend to have areas much larger than the arc length
-    # So, we will check for contour closure by checking if any of the contours
-    # have areas that are more than 10 times larger than the arc length
-    # This value may need to be adjusted with more testing.
+    
     ALTHRESH = 10
-    # We will slightly fill in the contours to make them easier to detect
     DILATE_PIXELS = 3
 
-    # assumes mapdata is uint8 and consists of 0 (unmapped), 1 (unoccupied),
-    # and other positive values up to 101 (occupied)
-    # so we will apply a threshold of 2 to create a binary image with the
-    # occupied pixels set to 255 and everything else is set to 0
-    # we will use OpenCV's threshold function for this
     ret,img2 = cv2.threshold(mapdata,2,255,0)
-    # we will perform some erosion and dilation to fill out the contours a
-    # little bit
+    
     element = cv2.getStructuringElement(cv2.MORPH_CROSS,(DILATE_PIXELS,DILATE_PIXELS))
-    # img3 = cv2.erode(img2,element)
     img4 = cv2.dilate(img2,element)
-    # use OpenCV's findContours function to identify contours
     fc = cv2.findContours(img4, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     contours = fc[0]
-    # find number of contours returned
     lc = len(contours)
-    # create array to compute ratio of area to arc length
     cAL = np.zeros((lc,2))
     for i in range(lc):
         cAL[i,0] = cv2.contourArea(contours[i])
