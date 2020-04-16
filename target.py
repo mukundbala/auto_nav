8d7ed832709f086763a5a8a6f16e5992a8b87877
Wang Huachen <huachenw24@gmail.com>
2020-04-16 15:17:33 +0800

    16 Apr test 3

--- target.py
+++ target.py
@@ -2,11 +2,14 @@
 
 
 import rospy
+import os
 import cv2
 import imutils
 import time
 import numpy as np
-import RPi.GPIO as GPIO
+import math
+import cmath
+#import RPi.GPIO as GPIO
 from PIL import Image
 from std_msgs.msg import String
 #from sensor_msgs.msg import Image
@@ -20,212 +23,211 @@ from imutils.video import VideoStream
 from collections import deque
 from cv_bridge import CvBridge, CvBridgeError
 
+cX = 1000
+cY = 1000
+
+
+def stopbot():
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    twist = Twist()
+    twist.linear.x = 0
+    twist.angular.z = 0
+    time.sleep(1)
+    pub.publish(twist)
 
 def get_odom_dir(msg):
-	global yaw
+    global yaw
 
-	orientation_quat =  msg.pose.pose.orientation
-	orientation_list = [orientation_quat.x, orientation_quat.y, orientation_quat.z, orientation_quat.w]
-    	(roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+    orientation_quat =  msg.pose.pose.orientation
+    orientation_list = [orientation_quat.x, orientation_quat.y, orientation_quat.z, orientation_quat.w]
+    (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
 
 
 def get_laserscan(msg):
-	global laser_range
+    global laser_range
 
-	laser_range = np.array(msg.ranges)
-	rospy.loginfo("Done getting ranges")
+    laser_range = np.array(msg.ranges)
 
 
 def move_forward():
-	global laser_range
+    global laser_range
 
-	pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-	rate = rospy.Rate(5)
+    # pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    # rate = rospy.Rate(5)
 
-        twist = Twist()
-	twist.linear.x = 0.05
-	twist.angular.z = 0.0
-	pub.publish(twist)
+    # twist = Twist()
+    # twist.linear.x = 0.05
+    # twist.angular.z = 0.0
+    # time.sleep(1)
+    # pub.publish(twist)
 
-	while True:
-		lr2 = (laser_range[range(0, 10, 1)]<0.4).nonzero()
-		if len(lr2) > 0:
-			break
+    # while True:
+    #     #rospy.loginfo("inside while loop")
+    #     lr2 = (laser_range[range(0, 7, 1)]<0.2).nonzero()
+    #     print(lr2[0])
+    #     if len(lr2[0]) > 0:
+    #         #rospy.loginfo("exited loop")
+    #         break
 
-	twist.linear.x = 0
-	twist.angular.z = 0
-	pub.publish(twist)
+    # twist.linear.x = 0
+    # twist.angular.z = 0
+    # pub.publish(twist)
+    os.system("rosnode kill /turtlebot3_lds")
+    rospy.loginfo("Finished moving")
 
-
-def pan():
-	global yaw, cX
-
-    	if cX - 150 == 0:
-    		return
-
-	else:
-    		rot_angle = math.atan(cX / 100)
-
-	twist = Twist()
-	pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-        rate = rospy.Rate(10)
-
-        current_yaw = np.copy(yaw)
-        c_yaw = complex(math.cos(current_yaw),math.sin(current_yaw))
-        target_yaw = rot_angle
-        c_target_yaw = complex(math.cos(target_yaw),math.sin(target_yaw))
-        rospy.loginfo('Desired Yaw: ' + str(math.degrees(cmath.phase(c_target_yaw))))
-        c_change = c_target_yaw / c_yaw
-        c_change_dir = np.sign(c_change.imag)
-        twist.linear.x = 0.0
-        twist.angular.z = c_change_dir * 0.1
-        pub.publish(twist)
-
-        c_dir_diff = c_change_dir
-        while(c_change_dir * c_dir_diff > 0):
-            current_yaw = np.copy(yaw)
-            c_yaw = complex(math.cos(current_yaw),math.sin(current_yaw))
-            c_change = c_target_yaw / c_yaw
-            c_dir_diff = np.sign(c_change.imag)
-            rate.sleep()
-
-        rospy.loginfo('End Yaw: ' + str(math.degrees(current_yaw)))
-        twist.angular.z = 0.0
-        pub.publish(twist)
-
-
-def tilt():
-	global cY
-
-	if cY - 150 == 0:
-		return
-	else:
-		tilt_angle = math.degrees(math.atan(cY / 100))
-
-	print(tilt_angle)
-	GPIO.setmode(GPIO.BOARD)
-	servo_pin = 12
-	GPIO.setup(servo_pin, GPIO.OUT)
-	p = GPIO.PWM(servo_pin, 50)
-	p.start(7.5)
-	actual = 2.5 + (tilt_angle * 10 /180)
-	p.ChangeDutyCycle(actual)
-	p.stop()
-
-
-def fire():
-	motor_pin = 40
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(21, GPIO.OUT)
-	GPIO.output(21, GPIO.HIGH)
-	time.sleep(7)
-	GPIO.output(21, GPIO.LOW)
-	GPIO.cleanup()
 
 class TargetDetector:
 
-	def __init__(self, colourLower, colourUpper):
+    def __init__(self, colourLower, colourUpper):
+        
+        self.lower = colourLower
+        self.upper = colourUpper
 
-		self.lower = colourLower
-		self.upper = colourUpper
+        self.t0 = time.time()
 
-		self.t0 = time.time()
+        self.target_point = Point()
 
-		self.target_point = Point()
-
-		#self.image_pub = rospy.Publisher("/target/image_target",Image,queue_size=1)
-		#self.mask_pub = rospy.Publisher("/target/image_mask",Image,queue_size=1)
-		#self.target_pub = rospy.Publisher("/target/point_target",Point,queue_size=1)
-		self.bridge = CvBridge()
-		self.image_sub = rospy.Subscriber("/raspicam_node/image/compressed",CompressedImage,self.callback, queue_size=1)
-
-
-	def callback(self, data):
-		global cX, cY
-
-		if data == None:
-			rospy.loginfo("Video not found")
-			rospy.logerr("LOL")
-			time.sleep(1000)
-
-		rospy.loginfo("Loaded video stream.")
-
-		np_arr = np.fromstring(data.data, np.uint8)
-		frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-		#frame = cv2.cvtColor(np.array(data), cv2.COLOR_RGB2BGR)
+        #self.image_pub = rospy.Publisher("/target/image_target",Image,queue_size=1)
+        #self.mask_pub = rospy.Publisher("/target/image_mask",Image,queue_size=1)
+        #self.target_pub = rospy.Publisher("/target/point_target",Point,queue_size=1)
+        self.bridge = CvBridge()
+        self.pan()
 
 
-		# resize the frame, blur it, and convert it to the HSV
-		# color space
-		frame = imutils.resize(frame, width=600)
-		blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-		hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    def pan(self):
+        global cX
 
-		# construct a mask for the color, then perform
-		# a series of dilations and erosions to remove any small
-		# blobs left in the mask
-		mask = cv2.inRange(hsv, self.lower, self.upper)
-		mask = cv2.erode(mask, None, iterations=2)
-		mask = cv2.dilate(mask, None, iterations=2)
-		#cv2.imshow("mask", mask), cv2.waitKey(1)
+        twist = Twist()
+        pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        tilt = rospy.Publisher('tilt', String, queue_size=10)
+        rate = rospy.Rate(10)
 
-		# find contours in the mask and initialize the current
-		# (x, y) center of the ball
-		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-			cv2.CHAIN_APPROX_SIMPLE)
-		cnts = imutils.grab_contours(cnts)
-		center = None
+        while not -50 <= cX <= 50:
+            self.image_sub = rospy.wait_for_message("/raspicam_node/image/compressed",CompressedImage)
+            self.callback(self.image_sub)
+            if cX == 1000:
+                rospy.logwarn("Target not found")
+            elif cX < 0:
+                rospy.loginfo("Panning right")
+                twist.angular.z = -0.1
+                time.sleep(0.5)
+                pub.publish(twist)
+            elif cX > 0:
+                rospy.loginfo("Panning left")
+                twist.angular.z = 0.1
+                time.sleep(0.5)
+                pub.publish(twist)
 
-		# only proceed if at least one contour was found
-		if len(cnts) > 0:
-			# find the largest contour in the mask, then use
-			# it to compute the minimum enclosing circle and
-			# centroid
-			c = max(cnts, key=cv2.contourArea)
-			((x, y), radius) = cv2.minEnclosingCircle(c)
-			M = cv2.moments(c)
-			cX = int(M["m10"] / M["m00"])
-			cY = int(M["m01"] / M["m00"])
-			print(cX + ', ' + cY)
-			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        rospy.loginfo("Finished panning")
+        twist.angular.z = 0
+        time.sleep(0.5)
+        pub.publish(twist)
+        tilt.publish('True')
+        
 
-			# only proceed if the radius meets a minimum size
-			if radius > 10:
-				# draw the circle and centroid on the frame,
-				# then update the list of tracked points
-				cv2.circle(frame, (int(x), int(y)), int(radius),
-					(0, 255, 255), 2)
-				cv2.circle(frame, center, 1, (0, 0, 255), -1)
 
-		# show the frame to our screen
-		cv2.imshow("Frame", frame), cv2.waitKey(1)
+    def tilt(self):
+        global cY
+
+        tilt_angle = math.degrees(math.atan(cY / 1000))
+        print(tilt_angle)
+        GPIO.setmode(GPIO.BOARD)
+        servo_pin = 12
+        GPIO.setup(servo_pin, GPIO.OUT)
+        p = GPIO.PWM(servo_pin, 50)
+        actual = 2.5 + (70 * 10 /180)
+        p.start(actual)
+        p.ChangeDutyCycle(actual)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(21, GPIO.OUT)
+        GPIO.output(21, GPIO.HIGH)
+        time.sleep(6)
+        GPIO.output(21, GPIO.LOW)
+        GPIO.cleanup()
+        
+
+    def callback(self, data):
+        global cX, cY
+
+        if data == None:
+            rospy.loginfo("Video not found")
+            rospy.logerr("LOL")
+            time.sleep(1000)
+
+        #rospy.loginfo("Loaded video stream.")
+
+        np_arr = np.fromstring(data.data, np.uint8)
+        frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        #frame = cv2.cvtColor(np.array(data), cv2.COLOR_RGB2BGR)
+
+
+        # resize the frame, blur it, and convert it to the HSV
+        # color space
+        frame = imutils.resize(frame, width=600)
+        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+        # construct a mask for the color, then perform
+        # a series of dilations and erosions to remove any small
+        # blobs left in the mask
+        mask = cv2.inRange(hsv, self.lower, self.upper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+        #cv2.imshow("mask", mask), cv2.waitKey(1)
+
+        # find contours in the mask and initialize the current
+        # (x, y) center of the ball
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        center = None
+
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+            x,y,w,h = cv2.boundingRect(c)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+            M = cv2.moments(c)
+            cX = int(M["m10"] / M["m00"] - 285)
+            cY = int(M["m01"] / M["m00"] - 100)
+            print(str(cX) + ', ' + str(cY))
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+
+        # show the frame to our screen
+        cv2.imshow("Frame", frame), cv2.waitKey(1)
 
 
 
 if __name__ == '__main__':
-	print("Enter color:")
-	coloor = raw_input()
-	if coloor == "red":
-		colourLower = (170, 100, 0)
-		colourUpper = (180, 255, 255)
-	elif coloor == "green":
-		colourLower = (40, 100, 0)
-		colourUpper = (80, 255, 255)
-	elif coloor == "blue":
-		colourLower = (100, 80, 0)
-		colourUpper = (140, 255, 255)
-	else:
-		rospy.loginfo("Color Invalid")
-	rospy.init_node('target_acquisition', anonymous=False)
-	rospy.loginfo("Node initialised")
-	rospy.Subscriber('scan', LaserScan, get_laserscan)
-	rospy.Subscriber('odom', Odometry, get_odom_dir)
-	rospy.loginfo("Subscribed to topics")
-	time.sleep(2)
-	move_forward()
-	td = TargetDetector(colourLower, colourUpper)
-	pan()
-	tilt()
-	fire()
+    rospy.on_shutdown(stopbot)
+    print("Enter color:")
+    coloor = raw_input()
+    if coloor == "red":
+        colourLower = (170, 100, 0)
+        colourUpper = (180, 255, 255)
+    elif coloor == "green":
+        colourLower = (40, 100, 0)
+        colourUpper = (80, 255, 255)
+    elif coloor == "blue":
+        colourLower = (100, 80, 0)
+        colourUpper = (140, 255, 255)
+    else:
+        rospy.loginfo("Color Invalid")
+    rospy.init_node('target_acquisition', anonymous=False, disable_signals=True)
+    rospy.loginfo("Node initialised")
+    rospy.Subscriber('scan', LaserScan, get_laserscan)
+    rospy.Subscriber('odom', Odometry, get_odom_dir)
+    time.sleep(2)
+    
+    try:
+        move_forward()
+        td = TargetDetector(colourLower, colourUpper)
+        time.sleep(5)
+        
 
-	rospy.spin()
+    except rospy.ROSInterruptException:
+        rospy.signal_shutdown("Shutting down")
+    rospy.spin()
