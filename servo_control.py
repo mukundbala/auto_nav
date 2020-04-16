d68b14f87b070d9c79e2dbb2cba1c6e62cc63668
Wang Huachen <huachenw24@gmail.com>
2020-04-16 15:17:02 +0800

    added pi node for target component

--- servo_control.py
+++ servo_control.py
@@ -1,62 +1,33 @@
-#eg2310
-# this code is used to control the servo
-
-import time
-import RPi.GPIO as GPIO
-
- 
-GPIO.setmode(GPIO.BCM)
-
-servo_pin = 18
-
-#set the pin as an output
-GPIO.setup(servo_pin, GPIO.OUT)
-
-#initialize the servo to be controlled by pwm with 50 HZ frequency
-p = GPIO.PWM(servo_pin, 50)
-
-#set servo to 90 degrees as it's starting position
-p.start(7.5)
-
-
-try:
-        while True:
-                x = input("Key in a number or exit by ctrl-c: ")
-#               while x != '-1':
-                angle = 2.5 + (x * 10.0 / 180.0)
-                p.ChangeDutyCycle(angle)
-                time.sleep(1)
-#                       x = input("Key in a number in degree or type '-1' to ex$
-
-except KeyboardInterrupt:
-        p.stop()
-        GPIO.cleanup()
 #!/usr/bin/env python
-#servo_control output PWM signal from the GPIO pin 18
+
+
+import rospy
 import time
 import RPi.GPIO as GPIO
+from std_msgs.msg import String
 
-GPIO.setmode(GPIO.BCM)
+go = False
+servo_pin = 12
+motor_pin = 21
 
-init = 18;
+def shoot():  
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(servo_pin, GPIO.OUT)
+    p = GPIO.PWM(servo_pin, 50)
+    actual = 2.5 + (70 * 10 /180)
+    p.start(actual)
+    p.ChangeDutyCycle(actual)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(motor_pin, GPIO.OUT)
+    GPIO.output(motor_pin, GPIO.HIGH)
+    time.sleep(6)
+    GPIO.output(motor_pin, GPIO.LOW)
+    p.stop
+    GPIO.cleanup()
 
-GPIO.setup(init, GPIO.OUT)
-
-#initialize pwm object with 1 kHz frequency
-x = input('Angle: ')
-
-pwm = GPIO.PWM(init, x) #the 1000 here need to be read in value from the main
-pwm.start(0)
-
-print("start")
-
-for i in range(0, 100, 1):
-	pwm.ChangeDutyCycle(i)
-	time.sleep(0.2)
-else: 
-	print("finished")
-
-
-#end
-pwm.stop()
-GPIO.cleanup()
+if __name__ == '__main__':
+    while not go:
+    sub = rospy.wait_for_message("tilt", String)
+    if sub == 'True':
+        break
+    shoot()
